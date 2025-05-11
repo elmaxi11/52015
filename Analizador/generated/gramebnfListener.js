008ba6c38b43fa9ff414dbd38043f2292e7b880a
Maximiliano Kotlik <maxikotlik11@gmail.com>
2025-05-11 12:32:30 -0300

    Add files via upload

---
+++ Analizador/generated/gramebnfListener.js
@@ -0,0 +1,99 @@
+// Generated from c:/Users/maxik/Desktop/Documentos/2025/Sintaxis/Unidad 4 ssl/Analizador/gramebnf.g4 by ANTLR 4.13.2
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by gramebnfParser.
+export default class gramebnfListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by gramebnfParser#programa.
+	enterPrograma(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#programa.
+	exitPrograma(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#bucle22.
+	enterBucle22(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#bucle22.
+	exitBucle22(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#salida.
+	enterSalida(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#salida.
+	exitSalida(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#mientr22.
+	enterMientr22(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#mientr22.
+	exitMientr22(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#sal22.
+	enterSal22(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#sal22.
+	exitSal22(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#cond22.
+	enterCond22(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#cond22.
+	exitCond22(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#caden22.
+	enterCaden22(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#caden22.
+	exitCaden22(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#letr.
+	enterLetr(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#letr.
+	exitLetr(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#digit.
+	enterDigit(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#digit.
+	exitDigit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by gramebnfParser#simbol.
+	enterSimbol(ctx) {
+	}
+
+	// Exit a parse tree produced by gramebnfParser#simbol.
+	exitSimbol(ctx) {
+	}
+
+
+
+}
---
+++ Analizador/index.js
@@ -0,0 +1,86 @@
+import gramebnfLexer from "./generated/gramebnfLexer.js";
+import gramebnfParser from "./generated/gramebnfParser.js";
+import { CustomgramebnfListener } from "./CustomgramebnfListener.js";
+import { CustomgramebnfVisitor } from "./CustomgramebnfVisitor.js";
+import antlr4, { CharStreams, CommonTokenStream, ParseTreeWalker } from "antlr4";
+import readline from 'readline';
+import fs from 'fs';
+
+class ThrowingErrorListener extends antlr4.error.ErrorListener {
+    syntaxError(recognizer, offendingSymbol, line, column, msg, err) {
+        throw new Error(`línea ${line}, columna ${column}: ${msg}`);
+    }
+}
+
+
+async function main() {
+    let input;
+
+    // Intento leer la entrada desde el archivo input - en forma sincrona.
+    try {
+        input = fs.readFileSync('input.txt', 'utf8');
+    } catch (err) {
+        // Si no es posible leer el archivo, solicitar la entrada del usuario por teclado
+        input = await leerCadena(); // Simula lectura síncrona
+        console.log(input);
+    }
+
+    try {
+    let inputStream = CharStreams.fromString(input);
+    let lexer = new gramebnfLexer(inputStream);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(new ThrowingErrorListener());
+
+
+    let tokenStream = new CommonTokenStream(lexer);
+    let parser = new gramebnfParser(tokenStream);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new ThrowingErrorListener());
+
+    let tree = parser.programa();
+
+    // Verifico si se produjeron errores
+        console.log("\nEntrada válida.");
+         console.log("\nTabla de Lexemas:");
+            tokenStream.fill(); // llena todos los tokens
+            tokenStream.tokens.forEach(token => {
+                const tokenName = gramebnfLexer.symbolicNames[token.type];
+                if (tokenName) {
+                    console.log(`${token.text} → ${tokenName}`);
+                }
+    });
+        const cadena_tree = tree.toStringTree(parser.ruleNames);
+        console.log(`\nÁrbol de derivación: ${cadena_tree}`);
+
+        // Utilizo un listener y un walker para recorrer el arbol e indicar cada vez que reconoce una sentencia (stat)
+    
+        // ParseTreeWalker.DEFAULT.walk(listener, tree);
+
+        // Utilizo un visitor para visitar los nodos que me interesan de mi arbol
+        const visitor = new CustomgramebnfVisitor();
+        visitor.visit(tree);
+    
+        
+    } catch (e) {
+    console.error("\n❌ Se detectó un error durante el análisis:");
+    console.error(e.message);
+    return; // importante: detener ejecución
+}
+}
+
+function leerCadena() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise(resolve => {
+        rl.question("Ingrese una cadena: ", (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
+
+// Ejecuta la función principal
+main();
